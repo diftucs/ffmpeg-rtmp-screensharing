2ecbd37a8a464237ba31daba733cef6dffe3670b
diftucs <95053553+diftucs@users.noreply.github.com>
2022-06-16 15:34:08 +0200

    Build against FFmpeg submodule

--- compile.sh
+++ compile.sh
@@ -1,1 +1,10 @@
-g++ -o main -g main.cpp -lavdevice -lswscale -lavformat -lavcodec -lavutil
+(
+    echo "Building FFmpeg" && \
+    cd ffmpeg/ && \
+    ./configure --enable-debug --enable-gpl --enable-libx264 --enable-shared && \
+    make
+)
+
+echo
+echo "Building main.cpp"
+g++ main.cpp -o main -g -Iffmpeg/libavfilter -Iffmpeg/libavcodec -Iffmpeg/libavdevice -Iffmpeg/libavformat -Iffmpeg/libavutil -Iffmpeg/libswresample -Iffmpeg/libswscale -Lffmpeg/libavfilter -Lffmpeg/libavcodec -Lffmpeg/libavdevice -Lffmpeg/libavformat -Lffmpeg/libavutil -Lffmpeg/libswresample -Lffmpeg/libswscale -lavfilter -lavcodec -lavdevice -lavformat -lavutil -lswresample -lswscale
